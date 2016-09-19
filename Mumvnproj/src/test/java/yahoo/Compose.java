721adfd20145187a4b43e045e5441ded5f2cb02b
venkatgn <venkatadrin.g@gmail.com>
2016-09-19 12:01:34 +0530

    dgdfgdfg

--- Mumvnproj/src/test/java/yahoo/Compose.java
+++ Mumvnproj/src/test/java/yahoo/Compose.java
@@ -24,6 +24,7 @@ public class Compose
  }
  public void sendmail() throws Exception
  {
+	 System.out.println("send mail");
 	 driver.findElement(By.xpath("//input[@class='composeicon']")).click();
 	 Thread.sleep(3000);
 	 driver.findElement(By.id("to")).sendKeys("abcd@gmail.com");
